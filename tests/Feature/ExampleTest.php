cd2819ffa646bb9e80ab655997bb05f2193bd3cb
Alexander Gorbovets <alexander.gorbovets@gmail.com>
2020-01-23 00:21:35 +0300

    Fixed feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,13 +2,15 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase {
 
+    use DatabaseMigrations;
+
     public function testBasicTest() {
-        $response = $this->get('http://uploadtest/');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
